93269cb816c50a872cc27c435fca30e90c3d3ee4
saulmamani <luas0_1@yahoo,es>
2020-04-27 00:36:18 -0400

    configuraciones iniciales, migraciones, seeders y model

---
+++ database/migrations/2020_04_26_202717_trabajador.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Trabajador extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trabajadores', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombre_completo", 200);
+            $table->string('url_documento')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trabajadores');
+    }
+}
